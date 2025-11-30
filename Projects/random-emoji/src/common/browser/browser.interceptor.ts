6d32253740a53420925896d4ed8489c9b5f4437a
Radu <radu.avadanii@gmail.com>
2025-11-30 18:23:33 +0200

    upd: interceptors.

---
+++ Projects/random-emoji/src/common/browser/browser.interceptor.ts
@@ -0,0 +1,23 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class BrowserInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest<Request>();
+    const userAgent = request.headers['user-agent'] || '';
+    const browserClient = userAgent.split(' ')[0] || 'Unknown Browser';
+    request.headers.browser = browserClient;
+    console.log(
+      `Interceptor manipulated request for browser: ${request.headers.browser}`,
+    );
+
+    return next.handle();
+  }
+}
--- Projects/random-emoji/src/main.ts
+++ Projects/random-emoji/src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AuthGuard } from './common/auth/auth.guard';
+import { BrowserInterceptor } from './common/browser/browser.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalGuards(new AuthGuard());
+  app.useGlobalInterceptors(new BrowserInterceptor());
 
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
