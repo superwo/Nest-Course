7b77dcd44de2347ad97a08f28db8d1361abcf7b8
Radu <radu.avadanii@gmail.com>
2025-12-01 18:26:51 +0200

    upd: config module.

--- Projects/nest-starter/src/app.service.ts
+++ Projects/nest-starter/src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService) {}
+
   getHello(): string {
+    const environmentVariable = this.configService.get<string>('environment');
+    console.log(`Current Environment: ${environmentVariable}`);
+
     return 'Hello World!';
   }
 }
