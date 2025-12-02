255c99a9008ed1d71090deb2127aa7e51898d419
Radu <radu.avadanii@gmail.com>
2025-12-02 09:52:52 +0200

    upd: transform interceptor.

--- Projects/nest-starter/src/core/interceptors/transform-response/transform-response.interceptor.ts
+++ Projects/nest-starter/src/core/interceptors/transform-response/transform-response.interceptor.ts
@@ -8,10 +8,26 @@ import { map, Observable } from 'rxjs';
 
 @Injectable()
 export class TransformResponseInterceptor implements NestInterceptor {
-  intercept<T>(
-    context: ExecutionContext,
-    next: CallHandler,
-  ): Observable<{ data: T }> {
-    return next.handle().pipe(map((data: T) => ({ data })));
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((response: unknown) => {
+        if (!response) {
+          return { data: [] };
+        }
+        if (
+          typeof response === 'object' &&
+          response !== null &&
+          'data' in response &&
+          'meta' in response
+        ) {
+          const resp = response as { data: unknown; meta: unknown };
+          return {
+            data: resp.data,
+            meta: resp.meta,
+          };
+        }
+        return { data: response };
+      }),
+    );
   }
 }
