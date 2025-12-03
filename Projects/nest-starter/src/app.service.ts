4b53d7ec96da7de6aca653bebbc45282e7b254cc
Radu <radu.avadanii@gmail.com>
2025-12-03 09:55:40 +0200

    init: logger.

--- Projects/nest-starter/src/app.service.ts
+++ Projects/nest-starter/src/app.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  private logger = new Logger(`AppService`);
   getHello() {
+    this.logger.log(`calling log from getHello`);
     return undefined;
   }
 }
